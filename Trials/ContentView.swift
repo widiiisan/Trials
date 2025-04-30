2d9450a82ae4495344c107c4a2df1b07d3a24d68
widi <widiasaniasw@gmail.com>
2025-04-30 21:14:49 +0700

    comment

--- Trials/ContentView.swift
+++ Trials/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, jkbgjabibhjajhd!")
             Text("Hello, jkbgjabibhjajhd!")
-            
+            //kdejodicnksjxk
         }
         .padding()
     }
