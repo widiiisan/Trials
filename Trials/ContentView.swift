7b547015db80b59e8dbb2320237b5652d3657b25
widi <widiasaniasw@gmail.com>
2025-04-30 20:21:44 +0700

    Initial Commit

---
+++ Trials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trials
+//
+//  Created by Widia Sania on 30/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trials/TrialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialsApp.swift
+//  Trials
+//
+//  Created by Widia Sania on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TrialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
